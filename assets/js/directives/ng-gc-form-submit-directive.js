0a2f90e9de9454e315517089bcff7f18a1664c1a
Philip Harrison <philip@mailharrison.com>
2014-05-16 18:24:51 +0100

    Serialise select and textarea, hack handle chat

--- assets/js/directives/ng-gc-form-submit-directive.js
+++ assets/js/directives/ng-gc-form-submit-directive.js
@@ -4,7 +4,6 @@ angular.module('ngGcFormSubmitDirective', [])
   .directive('ngGcFormSubmit', [
     '$window',
     function ngGcFormSubmitDirective($window) {
-
       /**
        * Serlialises all the fields in the specified form,
        * for posting in an AJAX request.
@@ -12,11 +11,11 @@ angular.module('ngGcFormSubmitDirective', [])
        * @param {FormElement} form
        * @return {Object} Form data
        */
-      function serialiseForm(form) {
+      function serialiseForm($form) {
         var values = {};
-        var inputs = form.getElementsByTagName('input');
+        var inputs = $form.find('input, select, textarea');
 
-        _.each(_.toArray(inputs), function(input) {
+        _.each(inputs, function(input) {
           // Ignore unselected checkboxes or radio buttons
           if (!((input.type === 'checkbox' || input.type === 'radio') &&
             !input.checked)) {
@@ -34,7 +33,7 @@ angular.module('ngGcFormSubmitDirective', [])
           };
 
           function onSubmit(event) {
-            var formValues = serialiseForm(event.target);
+            var formValues = serialiseForm($(event.target));
 
             var oldTitle = $window.document.title;
             document.title = 'Saving...';
@@ -47,6 +46,16 @@ angular.module('ngGcFormSubmitDirective', [])
               contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
               dataType: 'json'
             }).done(function done(response) {
+              // XXX HACK
+              // MOVE TO CALLBACK
+              if (response && response.chat === true) {
+                $window.location.search = '?chat=1';
+              }
+              // XXX HACK
+
+              // Scroll to top showing notification
+              window.scrollTo(0, 0);
+
               scope.$apply(function() {
                 scope.$isSuccess = true;
                 scope.$isError = false;
