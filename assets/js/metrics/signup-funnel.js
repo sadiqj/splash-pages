99fbd57f0d8eeed118b6df18043d6d233410e787
jamesshedden <james@jshedden.com>
2014-05-23 12:08:51 +0100

    Remove unused metrics js

--- assets/js/metrics/signup-funnel.js
+++ assets/js/metrics/signup-funnel.js
@@ -14,21 +14,8 @@ angular.module('ngGcSignupFunnel', [
       });
     });
 
-    ngGcPageRouter('/merchants/new', function(path) {
-      $window.gct('track', 'Pageview', {
-        'Path': path,
-        'Property': 'website',
-        'Type': 'sign up'
-      });
-    });
-
-    ngGcPageRouter('/connect/merchants/new', function(path) {
-      $window.gct('track', 'Pageview', {
-        'Path': path,
-        'Property': 'website',
-        'Type': 'sign up'
-      });
-    });
+    // "/merchants/new" and "/connect/merchants/new" are triggered in gocardless/gocardless
+    // assets/javascripts/connect/metrics
 
     // "Sign Up" event is fired in MerchantsController#create
 
