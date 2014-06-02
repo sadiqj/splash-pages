4e49a852de6e5150218da0da046a1ff788d47f8f
jamesshedden <james@jshedden.com>
2014-06-02 14:18:57 +0100

    Adding icons & retina images

---
+++ app/js/directives/ng-gc-smooth-scroll-directive.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var angular = require('angular');
+var _ = require('lodash');
+
+angular.module('ngGcSmoothScrollDirective', [])
+  .directive('ngGcSmoothScroll', [
+    '$window',
+    function ngGcSmoothScrollDirective($window) {
+      /**
+       * Serlialises all the fields in the specified form,
+       * for posting in an AJAX request.
+       *
+       * @param {jQueryElement} $form Form element
+       * @return {Object} Form values
+       */
+
+    }
+  ]);
