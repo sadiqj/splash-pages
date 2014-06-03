2396812318a0592c9ffbe9064b08a60051792edb
jamesshedden <james@jshedden.com>
2014-06-03 17:45:40 +0100

    Using attrs argument for smooth scroll directive

--- app/js/directives/ng-gc-smooth-scroll-directive.js
+++ app/js/directives/ng-gc-smooth-scroll-directive.js
@@ -4,11 +4,12 @@ angular.module('ngGcSmoothScrollDirective', []).directive('ngGcSmoothScroll', [
   function ngGcSmoothScrollDirective() {
 
     return {
-      link: function link(scope, element) {
+      link: function link(scope, element, attrs) {
         element.on('click', function(e) {
+          var href = attrs.href;
           e.preventDefault();
           $('html, body').animate({
-              scrollTop: $($.attr(this, 'href')).offset().top
+              scrollTop: $(href).offset().top
           }, 500);
           return false;
         });
