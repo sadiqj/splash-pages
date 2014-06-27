01667b9aedc4d35707893cbe6d04892840d9b78f
jamesshedden <james@jshedden.com>
2014-06-27 17:37:31 +0100

    Fixing specs

--- app/js/directives/ng-gc-randomize-team-directive.js
+++ app/js/directives/ng-gc-randomize-team-directive.js
@@ -6,16 +6,15 @@ angular.module('ngGcRandomizeTeamDirective', []).directive('ngGcRandomizeTeam', 
   function ngGcRandomizeTeamDirective() {
 
     return {
-      link: function link(scope, element, attrs) {
+      link: function link(scope, element) {
 
-        var teamArray = [];
         var teamMembers = element.find('.team-member').detach();
         var shuffled = _.shuffle(teamMembers);
         element.append(shuffled);
 
         // This element is invisible (but is still on page, to retain the correct height)
         // until the shuffling is done. (Note: no-js class means it's not invisible if there's no JS)
-        element.css("opacity", 1);
+        element.css('opacity', 1);
 
       }
     };
