d93074e0783c7cbf5211d9da1e7a44e999797210
Ken-Lo <ken@kenstechtips.com>
2015-02-02 17:48:14 +0000

    Tidy up code

--- scripts/nunjucks.js
+++ scripts/nunjucks.js
@@ -107,8 +107,8 @@ findFiles((argv.i || argv.input), function(filepath) {
   console.log('Compiling %s (%s)', filepath, outfile);
 
   stream(filepath, outfile, function(file, fileMetadata) {
-    var metadata = {};
-    _.extend(metadata, globalMetadata, fileMetadata);
+    var metadata = _.extend({}, globalMetadata, fileMetadata);
+	console.log(metadata);
     return compileTemplate(file, metadata);
   }).pipe(fs.createWriteStream(outfile));
 });
